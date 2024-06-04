d51ce3bc46ba9bb6838470af1a69cad4216ef593
SeungwonChoi <bestwondev@gmail.com>
2024-06-04 22:18:57 +0900

    [level 0] Title: 순서쌍의 개수, Time: 8.80 ms, Memory: 71.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120836. 순서쌍의 개수/순서쌍의 개수.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+
+        int i = 1;
+
+        for (i = 1; i <= n; i++) {
+            if (n % i == 0) {
+                answer += 1;
+            }
+        }
+        
+        return answer;
+    }
+}
