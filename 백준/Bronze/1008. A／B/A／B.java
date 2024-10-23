5dae621b63d3ab571c0cdb3f419ca39b0c0991fb
SeungwonChoi <bestwondev@gmail.com>
2024-10-23 22:57:49 +0900

    [Bronze V] Title: A/B, Time: 172 ms, Memory: 17752 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.java
@@ -0,0 +1,13 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+
+        double num1 = scanner.nextInt();
+        double num2 = scanner.nextInt();
+
+        System.out.println(num1 / num2);
+    }
+}
