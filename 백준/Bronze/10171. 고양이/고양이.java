e3374b051d816b8e6b31b98011a9593be23efe69
SeungwonChoi <bestwondev@gmail.com>
2024-10-23 23:49:15 +0900

    [Bronze V] Title: 고양이, Time: 100 ms, Memory: 14208 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.java
@@ -0,0 +1,11 @@
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        System.out.printf("\\    /\\\n");
+        System.out.printf(" )  ( ')\n");
+        System.out.printf("(  /  )\n");
+        System.out.printf(" \\(__)|\n");
+    }
+}
