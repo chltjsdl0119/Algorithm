0b17d8d98a8eaa3f3c7fe03e445c418e71a2dfcb
SeungwonChoi <bestwondev@gmail.com>
2024-10-23 23:13:36 +0900

    [Bronze V] Title: ??!, Time: 176 ms, Memory: 17712 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,13 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+
+        String str = scanner.nextLine();
+
+        System.out.println(str + "??!");
+
+    }
+}
