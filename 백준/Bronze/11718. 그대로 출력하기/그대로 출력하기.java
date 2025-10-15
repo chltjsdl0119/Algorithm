aad0745f21f4167ab8040e656a6baae4806f1d68
Seungwon-Choi <bestwondev@gmail.com>
2025-10-15 09:51:51 +0900

    [Bronze III] Title: 그대로 출력하기, Time: 108 ms, Memory: 14284 KB -BaekjoonHub

---
+++ 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        String input;
+
+        while ((input = reader.readLine()) != null) {
+            System.out.println(input);
+        }
+    }
+}
+
