702617c72097ba74b25863ad8e8d4e61bb271d4e
Seungwon-Choi <bestwondev@gmail.com>
2026-03-16 10:45:50 +0900

    [Bronze I] Title: 소인수분해, Time: 124 ms, Memory: 14312 KB -BaekjoonHub

---
+++ 백준/Bronze/11653. 소인수분해/소인수분해.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(reader.readLine());
+
+        if (N < 2) {
+            return;
+        }
+
+        for (int i = 2; i <= N; i++) {
+            while (N % i == 0) {
+                System.out.println(i);
+                N /= i;
+            }
+        }
+    }
+}
