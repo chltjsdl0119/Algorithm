bca4afb3c8330a2f0313c9ee982fcd12ea060101
Seungwon-Choi <bestwondev@gmail.com>
2026-03-17 11:28:39 +0900

    [Bronze III] Title: 네 번째 점, Time: 116 ms, Memory: 15888 KB -BaekjoonHub

---
+++ 백준/Bronze/3009. 네 번째 점/네 번째 점.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        int x = 0;
+        int y = 0;
+
+        for (int i = 0; i < 3; i++) {
+            StringTokenizer tokenizer = new StringTokenizer(reader.readLine());
+
+            x ^= Integer.parseInt(tokenizer.nextToken());
+            y ^= Integer.parseInt(tokenizer.nextToken());
+        }
+
+        System.out.println(x + " " + y);
+    }
+}
